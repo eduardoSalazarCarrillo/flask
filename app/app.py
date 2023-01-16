8b7b78cb9a289b33098b95c94d985f36e8b8beeb
eduardoSalazarCarrillo <122764571+eduardoSalazarCarrillo@users.noreply.github.com>
2023-01-15 22:11:43 -0600

    Update app.py

--- app/app.py
+++ app/app.py
@@ -48,5 +48,5 @@ def buildJSON(word):
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app.
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 # [END gae_flex_quickstart]
